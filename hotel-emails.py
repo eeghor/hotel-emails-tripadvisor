d2a96367e6eccc562772663c0ac2dc64864fdda2
eeghor <eeghor@gmail.com>
2017-07-11 16:11:46 +1000

    appears to work for multiple locations

--- hotel-emails.py
+++ hotel-emails.py
@@ -8,16 +8,15 @@ import time
 import sys
 import pandas as pd
 
+list_places = ["Sydney", "Melbourne", "Brisbane", "Perth", "Adelaide", "Hobart", "Canberra", "Darwin"]
 # full path to the webdriver to use; use webdriver.PhantomJS() for invisible browsing
 driver = webdriver.Chrome('/Users/ik/Codes/hotel-emails-tripadvisor/webdriver/chromedriver')
 # default waiting time
-WAIT_TIME = 10
+WAIT_TIME = 20
 # base URL
 BASE_REVIEW_URL = "http://tripadvisor.com.au/"
 # page to go to first
-start_page  = "https://www.tripadvisor.com.au/Best-Hotels-Sydney.html"
-driver.get(start_page)
-time.sleep(5)
+start_page  = "https://www.tripadvisor.com.au/Hotels"
 
 def _how_many_pages_on_pagination_bar():
 	"""
@@ -82,132 +81,133 @@ def _get_website():
 	except:
 		return None
 
-# pagination bar at the bottom of the page
-pagination_bar = WebDriverWait(driver, WAIT_TIME).until(EC.presence_of_element_located((By.CLASS_NAME, "pageNumbers")))
+for place in list_places:
 
-max_page = _how_many_pages_on_pagination_bar()
-
-print("[pagination bar]: available pages to visit: {}".format(max_page))
-
-
-# start visiting pages
-
-for page in range(1, max_page + 1):
-
-	print("page {}".format(page))
-
-	# first simply find that button
+	driver.get(start_page)  # the Hotels page
+	time.sleep(5)
+	# find the input and type in the place names
+	driver.find_element_by_class_name("typeahead_input").send_keys(place)
+	# find the find hotels button
+	driver.find_element_by_class_name("submit_text").click()
+	time.sleep(3)
 	# pagination bar at the bottom of the page
 	pagination_bar = WebDriverWait(driver, WAIT_TIME).until(EC.presence_of_element_located((By.CLASS_NAME, "pageNumbers")))
-
-	try:
-		page_button = pagination_bar.find_element_by_xpath("span[@data-page-number='" + str(page) + "']")
-		time.sleep(3)
-	except:
-		try:
-			page_button = pagination_bar.find_element_by_xpath("a[@data-page-number='" + str(page) + "']")
-		except:
-			print("[ERROR]: unable to find page {} button on the pagination bar!".format(page))
-			sys.exit(0)
-
-	#  if got to here, the button has been found; but it is clickable?
-	try:
-		time.sleep(3)
-		page_button.click()
-	except:
-		# give a warning but assume that it's because we are already on the page we need
-		print("[WARNING]: page button on the pagination bar is not clickable...")
-
-	# create a hotel url list for the hotels on this page
-	hname_lst = []
-	hurl_lst = []
-	hwebs_lst = []
-	haddr_lst = []
-	hemail_lst = []
-
-	time.sleep(3)
 	driver.switch_to_window(driver.window_handles[0])
-	# can we find any hotels on this page at all?
-	hrefs_on_page = driver.find_elements_by_xpath(".//a[@class='property_title']")
 
-	if not hrefs_on_page:
-		print("[ERROR]: cannot find any hotel links on this page...")
-		sys.exit(0)
-	else:
-		for a in hrefs_on_page:
-			hname_lst.append(a.text.lower().strip())
-			hurl_lst.append(a.get_attribute("href"))
+	max_page = _how_many_pages_on_pagination_bar()
 
-	print("found {} hotels on this page...".format(len(hname_lst)))
+	print("[pagination bar]: available pages to visit: {}".format(max_page))
 
+	# start visiting pages
 
-	for i, url in enumerate(hurl_lst[:2]):
+	for page in range(1, max_page + 1):
 
-		print("hotel {}/{}: {}".format(i + 1, len(hname_lst), hname_lst[i]))
-		print("url: {}".format(url))
+		print("page {}".format(page))
 
-		driver.execute_script("window.open('');")  # javascript because of some issues w Chrome - command + t may not work
-		driver.switch_to.window(driver.window_handles[-1])
-
-		driver.get(url)
-		time.sleep(3)
-
-		#  a d d r e s s
-		haddr_lst.append(_get_address())
-
-		# w e b s i t e
-		hwebs_lst.append(_get_website())
-
-		# e m a i l
-
-		# check if there's an option to email the hotel
+		# first simply find that button
+		# pagination bar at the bottom of the page
+		pagination_bar = WebDriverWait(driver, WAIT_TIME).until(EC.presence_of_element_located((By.CLASS_NAME, "pageNumbers")))
 
 		try:
-			email_hotel_option = WebDriverWait(driver, WAIT_TIME).until(EC.presence_of_element_located((By.CLASS_NAME, "email")))
-		except:
-			print("[WARNING]: can't find the email hotel option...")
-			hemail_lst.append(None)
-			print("closing driver")
-			print("handles now=", len(driver.window_handles))
-			driver.close()
-			print("handles now=", len(driver.window_handles))
-			print("switching to main window")
-			print("handles now=", len(driver.window_handles))
-			driver.switch_to_window(driver.window_handles[0])
-			print("moving on to next hotel url")
-			continue   # to next hotel URL
-
-		try:
-			email_hotel_option.click()
+			page_button = pagination_bar.find_element_by_xpath("span[@data-page-number='" + str(page) + "']")
 			time.sleep(3)
 		except:
-			print("[ERROR]: cannot click on the email option...")
-			sys.exit(0)
+			try:
+				page_button = pagination_bar.find_element_by_xpath("a[@data-page-number='" + str(page) + "']")
+			except:
+				print("[ERROR]: unable to find page {} button on the pagination bar!".format(page))
+				sys.exit(0)
 
-		hotel_email = None
-
+		#  if got to here, the button has been found; but it is clickable?
 		try:
-			alert = driver.switch_to.alert
+			time.sleep(3)
+			page_button.click()
 		except:
-			print("[ERROR]: cannot switch to alert!")
-			sys.exit(0)
+			# give a warning but assume that it's because we are already on the page we need
+			print("[WARNING]: page button on the pagination bar is not clickable...")
 
-		# if got here, the switch to alert went well
-		inps = WebDriverWait(driver,10).until(EC.presence_of_all_elements_located((By.ID, 'receiver')))
-		if not inps:
-			print("[ERROR]: cannot find any suitable inputs!")
+		# create a hotel url list for the hotels on this page
+		hname_lst = []
+		hurl_lst = []
+		hwebs_lst = []
+		haddr_lst = []
+		hemail_lst = []
+
+		time.sleep(3)
+		driver.switch_to_window(driver.window_handles[0])
+		# can we find any hotels on this page at all?
+		hrefs_on_page = driver.find_elements_by_xpath(".//a[@class='property_title']")
+
+		if not hrefs_on_page:
+			print("[ERROR]: cannot find any hotel links on this page...")
 			sys.exit(0)
 		else:
-			for inp in inps:
-				try:
-					hotel_email = inp.get_attribute("value").lower().strip()
-					hemail_lst.append(hotel_email)
-					print("email: {}".format(hemail_lst[-1]))
-					driver.close()
-					driver.switch_to.window(driver.window_handles[-1])
-				except:
-					print("[ERROR]: cannot pick the vaue of email input!")
-					hemail_lst.append(None)
-					sys.exit(0)
+			for a in hrefs_on_page:
+				hname_lst.append(a.text.lower().strip())
+				hurl_lst.append(a.get_attribute("href"))
+
+		print("found {} hotels on this page...".format(len(hname_lst)))
+
+		for i, url in enumerate(hurl_lst[:2]):
+
+			print("hotel {}/{}: {}".format(i + 1, len(hname_lst), hname_lst[i]))
+			print("url: {}".format(url))
+
+			driver.execute_script("window.open('');")  # javascript because of some issues w Chrome - command + t may not work
+			driver.switch_to.window(driver.window_handles[-1])
+
+			driver.get(url)
+			time.sleep(3)
+
+			#  a d d r e s s
+			haddr_lst.append(_get_address())
+
+			# w e b s i t e
+			hwebs_lst.append(_get_website())
+
+			# e m a i l
+			# check if there's an option to email the hotel
+			try:
+				email_hotel_option = WebDriverWait(driver, WAIT_TIME).until(EC.presence_of_element_located((By.CLASS_NAME, "email")))
+			except:
+				print("[WARNING]: can't find the email hotel option...")
+				hemail_lst.append(None)
+				driver.close()
+				driver.switch_to_window(driver.window_handles[0])
+				print("moving on to next hotel url")
+				continue   # to next hotel URL
+
+			try:
+				email_hotel_option.click()
+				time.sleep(3)
+			except:
+				print("[ERROR]: cannot click on the email option...")
+				sys.exit(0)
+
+			hotel_email = None
+
+			try:
+				alert = driver.switch_to.alert
+			except:
+				print("[ERROR]: cannot switch to alert!")
+				sys.exit(0)
+
+			# if got here, the switch to alert went well
+			inps = WebDriverWait(driver,10).until(EC.presence_of_all_elements_located((By.ID, 'receiver')))
+			if not inps:
+				print("[ERROR]: cannot find any suitable inputs!")
+				sys.exit(0)
+			else:
+				for inp in inps:
+					try:
+						hotel_email = inp.get_attribute("value").lower().strip()
+						hemail_lst.append(hotel_email)
+						print("email: {}".format(hemail_lst[-1]))
+						driver.close()
+						driver.switch_to.window(driver.window_handles[-1])
+					except:
+						print("[ERROR]: cannot pick the vaue of email input!")
+						hemail_lst.append(None)
+						sys.exit(0)
 
 driver.quit()
