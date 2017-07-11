35dcc4ed8693e0a2adb0b13791b857d79b756951
eeghor <eeghor@gmail.com>
2017-07-11 15:52:29 +1000

    working well for Sydney

--- hotel-emails.py
+++ hotel-emails.py
@@ -3,15 +3,15 @@ from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait, Select
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 import time
 import sys
 import pandas as pd
-from collections import defaultdict, namedtuple
 
 # full path to the webdriver to use; use webdriver.PhantomJS() for invisible browsing
 driver = webdriver.Chrome('/Users/ik/Codes/hotel-emails-tripadvisor/webdriver/chromedriver')
 # default waiting time
-WAIT_TIME = 40
+WAIT_TIME = 10
 # base URL
 BASE_REVIEW_URL = "http://tripadvisor.com.au/"
 # page to go to first
@@ -25,7 +25,7 @@ def _how_many_pages_on_pagination_bar():
 	"""
 	all_possible_bar_divs = driver.find_elements_by_xpath("//div[contains(@class,'unified') and contains(@class, 'standard_pagination')]")
 	max_page = None
-	time.sleep(3)
+
 	if all_possible_bar_divs:
 		for located_div in all_possible_bar_divs:
 			try:
@@ -41,6 +41,46 @@ def _how_many_pages_on_pagination_bar():
 	else:
 		print("[ERROR]: can't locate the pagination bar at the bottom...")
 		sys.exit(0)
+
+def _get_address():
+	"""
+	find and get a hotel address on the current page
+	"""
+
+	hotel_address = []
+
+	try:
+		span_address = WebDriverWait(driver, WAIT_TIME).until(EC.presence_of_element_located((By.CLASS_NAME, "street-address")))
+		hotel_address.append(span_address.text.lower().strip())
+	except:
+		print("[WARNING]: cannot find street address for {}..".format(hname_lst[i]))
+	try:
+		span_locality = WebDriverWait(driver, WAIT_TIME).until(EC.presence_of_element_located((By.CLASS_NAME, "locality")))
+		hotel_address.append(span_locality.text.lower().strip())
+	except:
+		print("[WARNING]: cannot find localty for {}..".format(hname_lst[i]))
+	try:
+		span_country = WebDriverWait(driver, WAIT_TIME).until(EC.presence_of_element_located((By.CLASS_NAME, "country-name")))
+		hotel_address.append(span_country.text.lower().strip())
+	except:
+		print("[WARNING]: cannot find country for {}..".format(hname_lst[i]))
+
+	return " ".join(hotel_address) if hotel_address else None
+
+def _get_website():
+	"""
+	in case there's a link to a hotel's website, collect it
+	"""
+		# see if there's a link to the hotel's website; if there is, click
+	try:
+		WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CLASS_NAME, "website"))).click()
+		# the website is supposed to start loading in a new window
+		driver.switch_to_window(driver.window_handles[-1])
+		driver.close()
+		driver.switch_to_window(driver.window_handles[-1])
+		return driver.current_url.split("//")[1].split("/")[0].lower().strip()
+	except:
+		return None
 
 # pagination bar at the bottom of the page
 pagination_bar = WebDriverWait(driver, WAIT_TIME).until(EC.presence_of_element_located((By.CLASS_NAME, "pageNumbers")))
@@ -59,11 +99,13 @@ for page in range(1, max_page + 1):
 	# first simply find that button
 	# pagination bar at the bottom of the page
 	pagination_bar = WebDriverWait(driver, WAIT_TIME).until(EC.presence_of_element_located((By.CLASS_NAME, "pageNumbers")))
+
 	try:
 		page_button = pagination_bar.find_element_by_xpath("span[@data-page-number='" + str(page) + "']")
+		time.sleep(3)
 	except:
 		try:
-			page_button = pagination_bar.find_element_by_xpath(".//a[@data-page-number='" + str(page) + "']")
+			page_button = pagination_bar.find_element_by_xpath("a[@data-page-number='" + str(page) + "']")
 		except:
 			print("[ERROR]: unable to find page {} button on the pagination bar!".format(page))
 			sys.exit(0)
@@ -82,6 +124,9 @@ for page in range(1, max_page + 1):
 	hwebs_lst = []
 	haddr_lst = []
 	hemail_lst = []
+
+	time.sleep(3)
+	driver.switch_to_window(driver.window_handles[0])
 	# can we find any hotels on this page at all?
 	hrefs_on_page = driver.find_elements_by_xpath(".//a[@class='property_title']")
 
@@ -95,60 +140,23 @@ for page in range(1, max_page + 1):
 
 	print("found {} hotels on this page...".format(len(hname_lst)))
 
+
 	for i, url in enumerate(hurl_lst[:2]):
 
 		print("hotel {}/{}: {}".format(i + 1, len(hname_lst), hname_lst[i]))
 		print("url: {}".format(url))
 
+		driver.execute_script("window.open('');")  # javascript because of some issues w Chrome - command + t may not work
+		driver.switch_to.window(driver.window_handles[-1])
+
 		driver.get(url)
-		time.sleep(5)
-		# hotel main page handle
-		hmain_h = driver.current_window_handle
+		time.sleep(3)
 
 		#  a d d r e s s
-
-		hotel_address = []
-
-		try:
-			span_address = WebDriverWait(driver, WAIT_TIME).until(EC.presence_of_element_located((By.CLASS_NAME, "street-address")))
-			hotel_address.append(span_address.text.lower().strip())
-		except:
-			print("[WARNING]: cannot find street address for {}..".format(hname_lst[i]))
-		try:
-			span_locality = WebDriverWait(driver, WAIT_TIME).until(EC.presence_of_element_located((By.CLASS_NAME, "locality")))
-			hotel_address.append(span_locality.text.lower().strip())
-		except:
-			print("[WARNING]: cannot find localty for {}..".format(hname_lst[i]))
-		try:
-			span_country = WebDriverWait(driver, WAIT_TIME).until(EC.presence_of_element_located((By.CLASS_NAME, "country-name")))
-			hotel_address.append(span_country.text.lower().strip())
-		except:
-			print("[WARNING]: cannot find country for {}..".format(hname_lst[i]))
-
-		haddr_lst.append(" ".join(hotel_address))
-
-		print("address: {}".format(haddr_lst[-1]))
+		haddr_lst.append(_get_address())
 
 		# w e b s i t e
-
-		# see if there's a link to the hotel's website; if there is, click
-		try:
-			WebDriverWait(driver, WAIT_TIME).until(EC.presence_of_element_located((By.CLASS_NAME, "website"))).click()
-			time.sleep(5)   # give it 5 sec to load
-			# the website is supposed to start loading in a new window
-			for h in driver.window_handles:
-				if h != hmain_h:
-					driver.switch_to_window(h)
-					hwebs_lst.append(driver.current_url.split("//")[1].split("/")[0].lower().strip())
-					driver.close()
-					driver.switch_to_window(hmain_h)
-		except:
-			hwebs_lst.append(None)
-
-		print("website: {}".format(hwebs_lst[-1]))
-
-		# hotel main page handle again
-		hmain_h = driver.current_window_handle
+		hwebs_lst.append(_get_website())
 
 		# e m a i l
 
@@ -159,7 +167,16 @@ for page in range(1, max_page + 1):
 		except:
 			print("[WARNING]: can't find the email hotel option...")
 			hemail_lst.append(None)
-			continue
+			print("closing driver")
+			print("handles now=", len(driver.window_handles))
+			driver.close()
+			print("handles now=", len(driver.window_handles))
+			print("switching to main window")
+			print("handles now=", len(driver.window_handles))
+			driver.switch_to_window(driver.window_handles[0])
+			print("moving on to next hotel url")
+			continue   # to next hotel URL
+
 		try:
 			email_hotel_option.click()
 			time.sleep(3)
@@ -176,7 +193,7 @@ for page in range(1, max_page + 1):
 			sys.exit(0)
 
 		# if got here, the switch to alert went well
-		inps = driver.find_elements(By.XPATH, "//input[@id='receiver']")
+		inps = WebDriverWait(driver,10).until(EC.presence_of_all_elements_located((By.ID, 'receiver')))
 		if not inps:
 			print("[ERROR]: cannot find any suitable inputs!")
 			sys.exit(0)
@@ -185,17 +202,12 @@ for page in range(1, max_page + 1):
 				try:
 					hotel_email = inp.get_attribute("value").lower().strip()
 					hemail_lst.append(hotel_email)
-					alert.dismiss()
-					break
+					print("email: {}".format(hemail_lst[-1]))
+					driver.close()
+					driver.switch_to.window(driver.window_handles[-1])
 				except:
 					print("[ERROR]: cannot pick the vaue of email input!")
 					hemail_lst.append(None)
 					sys.exit(0)
-
-			print("email: {}".format(hemail_lst[-1]))
-
-
-		# handles = driver.window_handles
-		# driver.switch_to_window(handles[0])
 
 driver.quit()
